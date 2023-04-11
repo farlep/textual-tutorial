96c7d89bed320037ca218faa2e8e8b92a87837c3
farlep <dracelia@tutanota.com>
2023-04-11 15:55:47 +0100

    Initial example

--- textual_tutorial/stopwatch.py
+++ textual_tutorial/stopwatch.py
@@ -1,13 +1,38 @@
 """Stopwatch exercise with Textual."""
 
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
+from textual.containers import Container
+from textual.widgets import Button, Header, Footer, Static
+
+class TimeDisplay(Static):
+    """A widget to display elapsed time."""
+
+class Stopwatch(Static):
+    """A stopwatch widget."""
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets."""
+        yield Button("Start", id = "start", variant = "success")
+        yield Button("Stop", id = "stop", variant = "error")
+        yield Button("Reset", id = "reset")
+        yield TimeDisplay("00:00:00.00")
 
 class StopwatchApp(App):
     """An app to manage stopwatches."""
 
-    BINDINGS=[("d", "toggle_dark", "Toggle dark mode")]
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+        yield Container(Stopwatch(), Stopwatch(), Stopwatch())
+
+    def action_toggle_dark(self) -> None:
+        """Toggle dark mode."""
+        self.dark = not self.dark
 
 def main() -> None:
     """Execute the program."""
-    print("Here")
+    app = StopwatchApp()
+    app.run()
